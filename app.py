af6af1b47be8bd6c9b3672fb5ef828e4da69c207
Anthony Christodoulou <christodoulou75@gmail.com>
2025-04-29 17:28:18 +0200

    git init

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cortex – QG Intelligent", layout="wide")
+st.title("🧠 Cortex – Ton Quartier Général Personnel")
+
+st.subheader("Bienvenue dans ton dashboard intelligent.")
